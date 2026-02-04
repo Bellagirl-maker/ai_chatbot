4cd333eddd31859f869c81f70d81e17814fe1d60
Isabella <isabellaotoo25@gmail.com>
2026-02-04 13:35:38 +0000

    Prepare app for production deployment

--- ai_chatbot_backend/app/controllers/application_controller.rb
+++ ai_chatbot_backend/app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   include ActionController::Cookies
 
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: -> { request.format.json? }
 
   def current_admin_user
     @current_admin_user ||= AdminUser.find_by(id: session[:admin_user_id])
