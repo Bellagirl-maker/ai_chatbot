cbf621438ee8e905efdfa16f7d8bd66c1b709ca9
Isabella <isabellaotoo25@gmail.com>
2026-02-06 13:19:14 +0000

    Remove invalid solid_* config

--- ai_chatbot_backend/config/application.rb
+++ ai_chatbot_backend/config/application.rb
@@ -1,25 +1,16 @@
 require_relative "boot"
-
 require "rails/all"
 
 Bundler.require(*Rails.groups)
 
 module AiChatbotBackend
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
     config.autoload_lib(ignore: %w[assets tasks])
 
+    # Session & cookies (needed for auth)
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
-
-    # 🔴 ADD THESE LINES BELOW
-    config.active_job.queue_adapter = :async
-
-    config.solid_queue.enabled = false
-    config.solid_cache.enabled = false
-    config.solid_cable.enabled = false
-    # 🔴 END ADD
   end
 end
